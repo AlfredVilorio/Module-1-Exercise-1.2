2d028799d4b17d53a597353d5e8c1fb1b09045da
AlfredVilorio <25935213-AlfredVilorio@users.noreply.replit.com>
2023-09-02 20:26:14 +0000

    Renamed program to Lyrics

---
+++ Lyrics.cs
@@ -0,0 +1,31 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) 
+  {
+    //Unit 1 Test
+    Console.WriteLine("Oh, well imagine/");
+    Console.WriteLine("As I'm packing the pews in a church corridor/");
+    Console.WriteLine("And I can't help but to hear/");
+    Console.WriteLine("No, I can't help but to hear an exchanging of words...");
+    Console.WriteLine("--------------------------------------------------------");
+  //Unit 2 Test
+    Console.WriteLine("Oh-oh-oh-oh-oh,");
+    Console.WriteLine("oh-oh-oh-oh,");
+    Console.WriteLine("oh-oh-oh/");
+    Console.WriteLine("Caught in a bad romance...");
+    Console.WriteLine("---------------------------------------------------------");
+  //Unit 3
+    Console.WriteLine("Coming out of my cage");
+    Console.WriteLine("And I've been doing just fine");
+    Console.WriteLine("Gotta gotta be down");
+    Console.WriteLine("Because I want it all");
+    Console.WriteLine("---------------------------------------------------------");
+  //Unit 4
+    Console.WriteLine("Now, this is story all bout how");
+    Console.WriteLine("My life got flipped");
+    Console.WriteLine("And I'd like to take a minute");
+    Console.WriteLine("Just sit right there");
+    Console.WriteLine("I'll tell you how I became the prince of a town called Bel-Air");
+ }
+}
